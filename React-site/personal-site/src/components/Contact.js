7686a11d58cc94b756ebadad143f06a16c310a9a
Krygore <webdev.krygore@gmail.com>
2023-09-27 19:09:25 -0700

    modified CSS on contact page

--- React-site/personal-site/src/components/Contact.js
+++ React-site/personal-site/src/components/Contact.js
@@ -15,10 +15,10 @@ export default function Contact() {
 
                 <h1>Contact Me</h1>
 
-                <h3>Email: <a href="mailto: webdev.krygore@gmail.com">webdev.krygore@gmail.com</a></h3>
-                <h3>Phone: <a href="tel:13602716385">(360)271-6385</a></h3>
-                <h3>LinkedIn: <a href="https://www.linkedin.com/in/webdev-michael/">Michael Wilcox</a></h3>
-                <h3>GitHub: <a href="https://github.com/WebDev-Michael">WebDev Michael</a></h3>
+                <h3>Email: <a className="contact-link" href="mailto: webdev.krygore@gmail.com">webdev.krygore@gmail.com</a></h3>
+                <h3>Phone: <a className="contact-link" href="tel:13602716385">(360)271-6385</a></h3>
+                <h3>LinkedIn: <a className="contact-link" href="https://www.linkedin.com/in/webdev-michael/">Michael Wilcox</a></h3>
+                <h3>GitHub: <a className="contact-link" href="https://github.com/WebDev-Michael">WebDev Michael</a></h3>
 
                 <h3>Location: Belfair, WA 98528</h3>
             </div>
