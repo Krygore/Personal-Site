c133c5a909c1d1c44c10d38772b9dea17a572c6e
Krygore <webdev.krygore@gmail.com>
2023-10-19 09:31:35 -0700

    added Kaleb Rupe Recommendation Letter

--- React-site/personal-site/src/components/References.js
+++ React-site/personal-site/src/components/References.js
@@ -6,6 +6,8 @@ export default function References () {
                 <a className="reference-link" target="blank" href="https://docs.google.com/document/d/1rumeLRsO4BoahyCUhZH7tomeBDmFD5rKtLpaNmfbGrM/edit">Kenneth Fisher</a>
 
                 <a className="reference-link" target="blank" href="https://docs.google.com/document/d/1CZqUmIC2nhmBiVLp5QAwXj-GD-B5KeIFeIBVKivFrKk/edit">Andria Kowalchik</a>
+
+                <a className="reference-link" target="blank" href="https://docs.google.com/document/d/1jvnfUHSLTaJUkXc7kI21dD6ElU4iDDwU8FEM-03FSCw/edit?usp=sharing">Kaleb Rupe</a>
             </div>
         </>
     )
